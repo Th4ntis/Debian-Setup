9065e456e4473efbaec44046888748d736212a2c
th4ntis <droneker610@protonmail.com>
2022-08-04 22:07:44 -0400

    Updated script

--- my-bs.sh
+++ my-bs.sh
@@ -120,10 +120,8 @@ yubico_install() {
 
 sublime_install() {
     echo -e "\n $greenplus Tnstalling sublime text editor \n"
-    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-    sudo apt install -y apt-transport-https
-    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-    sudo apt update && sudo apt upgrade -y
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo gpg --dearmor -o /usr/share/keyrings/sublimetext-keyring.gpg
+    sudo apt update
     sudo apt install -y sublime-text
     echo -e "\n $greenplus sublime install complete \n"
     sleep 2
