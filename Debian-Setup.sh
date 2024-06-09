7611644d52d82f9dbd88608f90d40be55391e7e4
Th4ntis <53808039+Th4ntis@users.noreply.github.com>
2024-06-09 18:29:06 +0000

    Update Debian-Setup.sh

--- Debian-Setup.sh
+++ Debian-Setup.sh
@@ -74,6 +74,7 @@ sudo systemctl enable pcscd
 wget -q https://developers.yubico.com/yubioath-flutter/Releases/yubico-authenticator-latest-linux.tar.gz -O ~/Yubikey.tar.gz
 tar -xf Yubikey.tar.gz && cd ~/yubico-authenticator*
 ./desktop_integration.sh install -i
+cd
 echo -e "$plus Complete"
 
 #echo -e "\n$plus Installing ProtonVPN..."
